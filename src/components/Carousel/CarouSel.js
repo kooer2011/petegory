48aeb1ff445f91d0d7a15cbc22521e841141dbba
kooer2011 <panyawut.k@ku.th>
2023-06-20 07:39:32 +0700

    edit css hove linke nav bar smoots

--- src/components/Carousel/CarouSel.js
+++ src/components/Carousel/CarouSel.js
@@ -11,28 +11,17 @@ function UncontrolledExample() {
     <Carousel>
       <Carousel.Item className="size-img">
         <img className="d-block w-100" src={imgs} alt="First slide" />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
+        <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item className="size-img">
         <img className="d-block w-100" src={imgs2} alt="Second slide" />
 
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
+        <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item className="size-img">
         <img className="d-block w-100" src={imgs3} alt="Third slide" />
 
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
+        <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
     </Carousel>
   );

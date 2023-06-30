00e922fa4331edf937e70fa3b9a8821431228ad0
tanapuminn <tanapumin.fo@gmail.com>
2023-07-01 01:04:34 +0700

    reset db

--- src/page/SignUp.js
+++ src/page/SignUp.js
@@ -4,56 +4,73 @@ import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import './LoginSignupContainer.css'
 import Swal from 'sweetalert2'
+import { message,Form,Input } from 'antd'
 
 const SignUp = () => {
-  const [data, setData] = useState({
-    name: '',
-    email: '',
-    phone: '',
-    password: ''
-  })
+  // const [data, setData] = useState({
+  //   name: '',
+  //   email: '',
+  //   phone: '',
+  //   password: ''
+  // })
 
   const navigate = useNavigate()
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    axios.post('http://localhost:8080/signup', data)
-      .then(res => {
-        if (res.data.Status === 'Success') {
-          const Toast = Swal.mixin({
-            toast: true,
-            position: 'bottom-start',
-            showConfirmButton: false,
-            timer: 3000,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-              toast.addEventListener('mouseenter', Swal.stopTimer)
-              toast.addEventListener('mouseleave', Swal.resumeTimer)
-            }
-          })
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   axios.post('http://localhost:8080/signup', data)
+  //     .then(res => {
+  //       if (res.data.Status === 'Success') {
+  //         const Toast = Swal.mixin({
+  //           toast: true,
+  //           position: 'bottom-start',
+  //           showConfirmButton: false,
+  //           timer: 3000,
+  //           timerProgressBar: true,
+  //           didOpen: (toast) => {
+  //             toast.addEventListener('mouseenter', Swal.stopTimer)
+  //             toast.addEventListener('mouseleave', Swal.resumeTimer)
+  //           }
+  //         })
 
-          Toast.fire({
-            icon: 'success',
-            title: 'Signed up successfully'
-          })
-          navigate('/login')
-        } else {
-          alert('Error')
-        }
-      })
-      .catch(err => console.log(err))
+  //         Toast.fire({
+  //           icon: 'success',
+  //           title: 'Signed up successfully'
+  //         })
+  //         navigate('/login')
+  //       } else {
+  //         alert('Error')
+  //       }
+  //     })
+  //     .catch(err => console.log(err))
+  // }
+
+  //formhandler
+  const finishHandler = async (values) => {
+    try {
+      const res = await axios.post('/user/signup', values)
+      if(res.data.success) {
+        message.success('Signup Successfully')
+        navigate('/login')
+      } else {
+        message.error(res.data.message)
+      }
+    } catch (error) {
+      console.log(error)
+      message.error('Someting Went Wrong')
+    }
   }
 
   return (
     <div className='signup'>
       <h1>SignUp</h1>
-      <form onSubmit={handleSubmit}>
-        <input type='text' id='inputName' placeholder='Name' onChange={e => setData({ ...data, name: e.target.value })} />
-        <input type='email' id='inputEmail' placeholder='Email' onChange={e => setData({ ...data, email: e.target.value })} />
-        <input type='number' id='inputNumber' placeholder='Phone Number' onChange={e => setData({ ...data, phone: e.target.value })} />
-        <input type='password' id='inputPassword' placeholder='Password' onChange={e => setData({ ...data, password: e.target.value })} />
-        <button type={'submit'} >SignUp</button>
-      </form>
+      <Form onFinish={finishHandler}>
+        <Input type='text' placeholder='Name' required  /> 
+        <Input type='email' placeholder='Email' required  />
+        <Input type='number' placeholder='Phone Number' required  />
+        <Input type='password' placeholder='Password' required  />
+        <button type='submit' >SignUp</button>
+      </Form>
     </div>
   )
 }
